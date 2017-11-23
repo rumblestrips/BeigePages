927728aa5c574f8dc4cab457203be23f758a6276
Roger Suffling <rsuffling@yahoo.co.uk>
2017-11-23 16:18:33 +0000

    UPDATE - Data structure added for lookup and register
     Data structure added for lookup and register.

--- lookup/main.go
+++ lookup/main.go
@@ -7,21 +7,34 @@ import (
 )
 
 func main() {
+	phoneRegistry := make(map[string]string)
+
 	router := gin.Default()
 
 	// This handler will match /user/john but will not match neither /user/ or /user
-	router.GET("/lookup/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		phoneNumber := "0123456"
+	router.GET("/lookup/:name", lookup(phoneRegistry))
+	router.POST("/register/:name/:phoneNumber", register(phoneRegistry))
+	router.Run(":8080")
+}
 
-		if name == "daniel" {
+func lookup(phoneRegistry map[string]string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		phoneNumber, ok := phoneRegistry[c.Param("name")]
+
+		if ok {
 			c.JSON(http.StatusOK, gin.H{
 				"phoneNumber": phoneNumber,
 			})
 		} else {
 			c.Status(http.StatusNotFound)
 		}
-	})
+	}
+}
 
-	router.Run(":8080")
+func register(phoneRegistry map[string]string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		phoneRegistry[c.Param("name")] = c.Param("phoneNumber")
+
+		c.Status(http.StatusOK)
+	}
 }

29b1e130ac01064832c1a126b62fddf4ef2f311a
Tom Connolly <tom@tconnolly.co.uk>
2017-11-24 14:42:23 +0000

    hacky prometheus

--- lookupService/main.go
+++ lookupService/main.go
@@ -8,8 +8,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var phoneRegistry map[string]string
+var numLookups int
+var numRegistrations int
+
 func main() {
-	phoneRegistry := make(map[string]string)
+	phoneRegistry = make(map[string]string)
+	numLookups = 0
+	numRegistrations = 0
 
 	router := gin.Default()
 
@@ -20,29 +26,30 @@ func main() {
 	port = fmt.Sprintf(":%s", port)
 
 	// This handler will match /user/john but will not match neither /user/ or /user
-	router.GET("/lookup/:name", lookup(phoneRegistry))
-	router.POST("/register/:name/:phoneNumber", register(phoneRegistry))
+	router.GET("/lookup/:name", lookup)
+	router.POST("/register/:name/:phoneNumber", register)
+	router.GET("/metrics", func(c *gin.Context) {
+		c.String(http.StatusOK, fmt.Sprintf("num_lookups %d\nnum_registrations %d", numLookups, numRegistrations))
+	})
 	router.Run(port)
 }
 
-func lookup(phoneRegistry map[string]string) func(*gin.Context) {
-	return func(c *gin.Context) {
-		phoneNumber, ok := phoneRegistry[c.Param("name")]
+func lookup(c *gin.Context) {
+	numLookups++
+	phoneNumber, ok := phoneRegistry[c.Param("name")]
 
-		if ok {
-			c.JSON(http.StatusOK, gin.H{
-				"phoneNumber": phoneNumber,
-			})
-		} else {
-			c.Status(http.StatusNotFound)
-		}
+	if ok {
+		c.JSON(http.StatusOK, gin.H{
+			"phoneNumber": phoneNumber,
+		})
+	} else {
+		c.Status(http.StatusNotFound)
 	}
 }
 
-func register(phoneRegistry map[string]string) func(*gin.Context) {
-	return func(c *gin.Context) {
-		phoneRegistry[c.Param("name")] = c.Param("phoneNumber")
+func register(c *gin.Context) {
+	numRegistrations++
+	phoneRegistry[c.Param("name")] = c.Param("phoneNumber")
 
-		c.Status(http.StatusOK)
-	}
+	c.Status(http.StatusOK)
 }

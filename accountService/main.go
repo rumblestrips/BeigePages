07e6ae79d126d664c5e260e13d28dee6078bc0dc
Tom Connolly <tom@tconnolly.co.uk>
2017-11-24 12:11:20 +0000

    added account struct & account service lookup endpoint

--- accountService/main.go
+++ accountService/main.go
@@ -8,8 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type account struct {
+	name        string
+	phoneNumber string
+	postCode    string
+	email       string
+}
+
 func main() {
-	accountRegistry := make(map[string]string)
+	accountRegistry := make(map[string]account)
 
 	router := gin.Default()
 
@@ -20,14 +27,34 @@ func main() {
 	port = fmt.Sprintf(":%s", port)
 
 	// This handler will match /user/john but will not match neither /user/ or /user
-	router.POST("/accountService/:name/:phoneNumber", register(accountRegistry))
+	router.POST("/accountService/:name/:phoneNumber/:postCode/:email", register(accountRegistry))
+	router.GET("/accountService/:name", lookup(accountRegistry))
 	router.Run(port)
 }
 
-func register(accountRegistry map[string]string) func(*gin.Context) {
+func register(accountRegistry map[string]account) func(*gin.Context) {
 	return func(c *gin.Context) {
-		accountRegistry[c.Param("name")] = c.Param("phoneNumber")
+		newAccount := account{
+			name:        c.Param("name"),
+			phoneNumber: c.Param("phoneNumber"),
+			postCode:    c.Param("postCode"),
+			email:       c.Param("email"),
+		}
+
+		accountRegistry[newAccount.name] = newAccount
 
 		c.Status(http.StatusOK)
+	}
+}
+
+func lookup(accountRegistry map[string]account) func(*gin.Context) {
+	return func(c *gin.Context) {
+		acc, ok := accountRegistry[c.Param("name")]
+
+		if ok {
+			c.JSON(http.StatusOK, acc)
+		} else {
+			c.Status(http.StatusNotFound)
+		}
 	}
 }
